db0b051bb0339491654e1d98285c413ed8810eb8
rogelioa3738 <149015162+rogelioa3738@users.noreply.github.com>
2024-07-31 12:37:32 +1200

    Add files via upload

--- banner_nav.php
+++ banner_nav.php
@@ -18,7 +18,7 @@
     <h2>Search</h2>
 
 <!-- Show All Button -->
-<a class="search-button" href="show_all.php">
+<a class="search-button" href="show_all.php" style="text-decoration:none;">
 <div class="button-link full-button">
     <span class="button-text">Show all</span>
     <span class="button-emoji"><i class="fas fa-search fa-lg"></i></span>
@@ -26,12 +26,73 @@
 </a>
 
 <!-- Random Button -->
-<a class="search-button" href="random.php">
+<a class="search-button" href="random.php" style="text-decoration:none;">
 <div class="button-link full button">
     <span class="button-text">Random</span>
     <span class="button-emoji"><i class="fas fa-random fa-lg"></i></span>
 </div>
 </a>
 
+<hr>
+		
+<!-- Keyword Area -->
+<form method="post" action="keyword.php" enctype="multi/form-data">
+	
+	<div class="search-container">
+		<input class="search" type="text" name="keyword_search" required valuez="" placeholder="keyword">
+		
+	<button class="button-link" type="submit" name="keyword">
+		<span class="small-mag"><i class="fas fa-search fa-lg"></i><span>
+	</button>
+		
+	</div>  <!-- / search container -->
+	
+</form>
 
-    </div>  <!-- / nav -->
+<!-- Topic Search -->
+<form class="search-form" method="post" action="topic.php" enctype="multipart/form-data">
+
+    <div class="search-container">
+
+        <!-- Topic Drop down goes here... -->
+        <select class="search" required>
+            <option value="">>Choose../</option>
+            <option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+        </select>
+
+        <button class="button-link" type="submit" name="keyword">
+            <span class="small-mag"><i class="fas fa-search fa-lg"></i></span>
+        </button>
+
+    </div>  <!-- / seach div for topic box -->
+
+</form>
+
+<hr />
+
+<!-- Date Area -->
+<!-- Today Button -->
+<a class="search-button" href="today.php">
+<div class="button-link full-button">
+    <span class="button-text">Today</span>
+    <span class="button-emoji"><i class="fas fa-calendar-week fa-lg"></i></span>
+</div>
+</a>
+    
+<!-- Date form -->
+<form class="search-form" method="post" action="topic.php" enctype="multipart/form-data">
+
+    <div class="search-container">
+
+        <input class="search" type="date" name="date-search" required value=""/>
+
+        <button class="button-link" type="submit" name="date-search">
+            <span class="small-mag"><i class="fas fa-search fa-lg"></i></span>
+        </button>
+
+    </div>  <!-- search container for date form -->
+
+</form>
+
+</div>  <!-- / nav -->
